8b610b5fd8bdd740ef76fb35c363332052d8a720
Shugeeth Babu <meetshugeeth@gmail.com>
2021-04-17 03:06:09 +0530

    Add bootstrap, reactstrap and react-popper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+import { Component } from "react";
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <NavbarBrand href="/"><img src={logo} className="App-logo" alt="logo" /> Restaurant Confusion</NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
