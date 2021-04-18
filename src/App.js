6dea1deee09ba10285421af105bd0171a66d8812
Shugeeth Babu <meetshugeeth@gmail.com>
2021-04-18 22:13:29 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
   render(){
     return (
       <div>
         <Navbar dark color="primary">
           <NavbarBrand href="/"><img src={logo} className="App-logo" alt="logo" /> Restaurant Confusion</NavbarBrand>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
