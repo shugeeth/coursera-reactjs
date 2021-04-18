67c16947830640afc21ae5e360b18ab963aa2127
Shugeeth Babu <meetshugeeth@gmail.com>
2021-04-18 12:42:19 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Component } from "react";
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <NavbarBrand href="/"><img src={logo} className="App-logo" alt="logo" /> Restaurant Confusion</NavbarBrand>
         </Navbar>
+        <Menu />
       </div>
     );
   }
