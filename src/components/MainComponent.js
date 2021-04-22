3b0670d1001e3ce397177b456c93b03a11add456
Shugeeth Babu <meetshugeeth@gmail.com>
2021-04-22 20:44:18 +0530

    Week 2 Assignment - About Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,6 +13,7 @@ import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props){
@@ -50,7 +51,8 @@ class Main extends Component {
                 dishClick={(dishId) => this.onDishSelect(dishId)} /> */}
             <Switch>
               <Route path="/home" component={HomePage} />
-              <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}/> } />
+              <Route path="/aboutus" component={()=><About leaders={this.state.leaders}/>} />
+              <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/> } />
               <Route path="/menu/:dishId" component={DishWithId} />
               <Route path="/contactus" component={Contact} />
               <Redirect to="/home" />
